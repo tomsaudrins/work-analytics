0ff3d98883d9fc13ab4c11e750b002e4e8d8b146
Toms Audrins <toms.audrins@gmail.com>
2021-08-22 18:53:12 +0200

    add selection page for a new file or data sample

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import OptionCard from "./OptionCard";
+
 const App = () => {
-  return <div className="App"></div>;
+  const selectFile = () => {
+    let input = document.createElement("input");
+    input.type = "file";
+    input.click();
+  };
+
+  const proceed = () => {
+    console.log("Proceed");
+  };
+
+  return (
+    <div className="App">
+      <h1 className="header">Work hour visualizer!</h1>
+      <OptionCard
+        title="Upload a new file"
+        button_text="Select file..."
+        onClick={selectFile}
+      />
+      <OptionCard
+        title="Use our data sample"
+        button_text="Next"
+        onClick={proceed}
+      />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/OptionCard.js
@@ -0,0 +1,12 @@
+const OptionCard = ({ title, button_text, onClick }) => {
+  return (
+    <div className="card">
+      <h2 className="card-header">{title}</h2>
+      <button className="select-button" onClick={onClick}>
+        {button_text}
+      </button>
+    </div>
+  );
+};
+
+export default OptionCard;
